9af67a7a57ccf11d7e17bf2ce3c1800fd9d6c08c
viniciusdepaula <viniciusdpaula@gmail.com>
2017-08-19 10:28:41 -0300

    Labs JavaFX

---
+++ Lab08ConversorTemperatura/src/br/edu/unitri/ConversorTemperaturaController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.unitri;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ConversorTemperaturaController implements Initializable {
+    
+    @FXML
+    private TextField txtTemp;
+    @FXML
+    private Button btnConverter;
+    @FXML
+    private Label lblResultado;
+     
+    @FXML
+    private void converterCelsiusFahrenheit(ActionEvent event) {
+        
+        double tempFahr = (Double.parseDouble(txtTemp.getText()) * 1.8 + 32);
+        lblResultado.setText(tempFahr + " Fahrenheit");
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) { 
+    }      
+}
